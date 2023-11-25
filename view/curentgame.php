4d8adcd577c19940b4242e583b0c974befbe7857
math <mathis.pause501@gmail.com>
2023-11-25 15:29:24 +0100

    modification mineur

---
+++ view/curentgame.php
@@ -0,0 +1,70 @@
+<div id="mySidenav" class="sidenav">
+    <button class="openbtn" onclick="openinventaireC()" id="Carte">Carte</button>
+    <button class="openbtn" onclick="openinventaireI()" id="Item">Item</button>
+</div>
+<div id="inventairecarte" class="sidepanel">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeinventaireC()">×</a>
+    <div class="centre">
+        <table class="Menuconsomable">
+            <tbody>
+                <tr>
+                    <td>test</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+<div id="inventaireitem" class="sidepanel">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeinventaireI()">×</a>
+    <div class="centre">
+        <table class="Menuconsomable">
+            <tbody>
+                <tr>
+                    <td>test</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+
+
+
+
+<script>
+    function openinventaireC() {
+        document.getElementById("inventairecarte").style.width = "100%";
+    }
+
+    function closeinventaireC() {
+        document.getElementById("inventairecarte").style.width = "0";
+    }
+
+    function openinventaireI() {
+        document.getElementById("inventaireitem").style.width = "100%";
+    }
+
+    function closeinventaireI() {
+        document.getElementById("inventaireitem").style.width = "0";
+    }
+</script>
